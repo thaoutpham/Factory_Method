7cf94978ddff0c8a4f074f21b6f50b35dd701c24
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-06-07 13:51:55 +0700

    [Thực hành] Factory Method

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        FactoryAnimal animalFactory=new FactoryAnimal();
+        Animal a1=animalFactory.getAnimal("feline");
+        System.out.println("a1 sound: "+a1.makeSound());
+        Animal a2 = animalFactory.getAnimal("canine");
+        System.out.println("a2 sound: " + a2.makeSound());
+    }
+}
